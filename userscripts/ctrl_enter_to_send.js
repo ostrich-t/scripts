146157c27dde9acda14eb3df42588aaed55b2ac4
ostrich-t <t.nakajima.busi@gmail.com>
2023-11-10 01:12:11 +0900

    update chatGPT userscripts

--- userscripts/ctrl_enter_to_send.js
+++ userscripts/ctrl_enter_to_send.js
@@ -1,8 +1,9 @@
 // ==UserScript==
-// @name         ctrl+Enter to send
+// @name         ChatGPT Extension
 // @namespace    http://chat.openai.com/
 // @version      0.1
 // @description  Press enter to start a new line, and ctrl+enter to send.(On mac, cmd+enter to send).
+// @description  Press / to focus on the prompt textarea.
 // @author       Takumu Nakajima
 // @match        https://chat.openai.com/c/*
 // @grant        none
@@ -10,6 +11,18 @@
 
 (function() {
     'use strict';
+
+    document.addEventListener('keydown', (event) => {
+      if (event.key === '/' && !event.target.matches('input, textarea')) {
+        event.preventDefault();
+
+        const inputElement = document.getElementById('prompt-textarea');
+
+        if (inputElement) {
+          inputElement.focus();
+        }
+      }
+    });
 
     document.querySelector('textarea')
       .addEventListener('keydown', (e) => {
